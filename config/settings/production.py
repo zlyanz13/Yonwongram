f7f6259f0b07473fd7379b9ac9fb40ea23802317
zlyanz13 <Yn_Wnz@DESKTOP-72N3L2J.localdomain>
2018-11-17 20:39:57 +0900

    no sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -158,8 +158,8 @@ MIDDLEWARE = ['raven.contrib.django.raven_compat.middleware.SentryResponseErrorI
 
 # Sentry
 # ------------------------------------------------------------------------------
-SENTRY_DSN = env('DJANGO_SENTRY_DSN')
-SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
+#SENTRY_DSN = env('DJANGO_SENTRY_DSN')
+#SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
